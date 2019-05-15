62fb6be9dc79ab247be5e3022bb00dca20e2aa30
Vaughn <vgree007@gmail.com>
2019-05-15 21:42:29 +0000

    third push

--- README.md
+++ README.md
@@ -1,7 +1,8 @@
-
 The name of our product is FilmNation.
 
 The members of this team are Vaughn Greenbridge and Nathan Alleyne.
 
-If you want to learn all there is to learn about movies that you may have forgotten about or just know nothing about,
-here's the website to use.
+If you want to learn all there is to learn about movies that you may have forgotten
+about or just know nothing about, here's the website to use. We used the Movie OMDb
+API to recieve our information that we displayed when the user presses 
+search.
--- js/script.js
+++ js/script.js
@@ -1,16 +1,18 @@
 /*global $*/
 
-$("#searchbutton").click(function(){
-    
+$(".button").click(function(){
+    console.log("console")
     var search = $("#search").val();
-    console.log(search)
     $.ajax({
-    url: "https://www.omdbapi.com/?apikey=8e9ded79&y=2016&s=" + search,
+    url: "https://www.omdbapi.com/?apikey=8e9ded79&plot=full&t=" + search,
     method: "GET",
     success: function(response){
-        $("body").append("<h2>" + response[0].Title + "</h2>" +
-                        "<p> This movie was released in " + response[0].Year + "</p>");
-        console.log(response)
+         console.log(response);
+
+        $("#results").html("<h2>" + response.Title + "</h2>" 
+        + "<img src=" + response.Poster + ">" +
+        "<p> This movie was released in " + response.Year + ".</p>" +
+        "<p>" + response.Plot  + "</p>");
         
     },
 
